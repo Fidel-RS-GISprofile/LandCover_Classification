c694c2a9c2bc307bd43337fff232597d0ac15d7b
Fidel <fideluwizeye@gmail.com>
2021-03-19 10:06:49 +0300

    update

--- Segmentation/segmentation_otb_lsms.R
+++ Segmentation/segmentation_otb_lsms.R
@@ -23,13 +23,20 @@ segt_dir <- paste0(workdir,"/segments/")
 if (!dir.exists(segt_dir))
   dir.create(segt_dir) #||Create a directory
 
-system(paste0(workdir,"/ug500_2019stnl2_6bands.tif")) #||Check access for the raster multiband file
+mosaic_file  <- "tmp/smooth_3_10_0.1_100_10.tif"
+segment_file <- "segments/tile1_segments.shp"
+segment_tif  <- "segments/tile1_segments.tif"
+scriptdir    <- "~/obia/scripts/"
+
+system(paste0(workdir,"/WCS_AOI_/WCS_AOI_ls7542010.tif")) #||Check access for the raster multiband file
+## Start processing
+startTime <- Sys.time(); cat("Start time", format(startTime),"\n")
 #||PERFORM SEGMENTATION USING THE OTB-SEG ALGORITHM
-params <- c(5, # radius of smoothing (pixels) ||3 for Landsat or 5 for Sentinel 2 UGA
-            200, # radius of proximity (pixels)||150 for Landsat or 300 for Sentinel 2 UGA
+params <- c(3, # radius of smoothing (pixels) ||3 for Landsat or 5 for Sentinel 2 UGA
+            10, # radius of proximity (pixels)||150 for Landsat or 300 for Sentinel 2 UGA
             0.1, # radiance threshold
             100, # iterations of algorithm
-            50) # segment minimum size (pixels)||10=1hectare Landsat or 50=1/2hectare sentinel 2 UGA
+            10) # segment minimum size (pixels)||10=1hectare Landsat or 50=1/2hectare sentinel 2 UGA
 #||Apply KMEAN shift algorith to raster band to do clustering
 system(sprintf("otbcli_MeanShiftSmoothing -in %s -fout %s -foutpos %s -spatialr %s -ranger %s -thres %s -maxiter %s",
                "ug500_2019ls8_5bands.tif",
@@ -55,3 +62,27 @@ system(sprintf("rm %s",
                paste0(tmpdir,"tmp*.tif")
 ))
 print("***** Segmentaion complete *****")#||Notice for completion of the segmentation process
+
+################### PROCESS - SEGMENTATION AS SHP
+system(paste("otbcli_Segmentation -filter.meanshift.spatialr %s -filter.meanshift.ranger %s -mode.vector.minsize 5 %s -mode.vector.tilesize 4096", 
+             "-in",mosaic_file,
+             "-mode vector",
+             "-mode.vector.out",segment_file,
+             "-filter meanshift",
+             "-mode.vector.outmode ovw",
+             params[1],
+             params[2],
+             sep=" "))
+
+
+################### PROCESS - CONVERT SEGMENTATION AS TIF
+system(sprintf("python3 %s/oft-rasterize_attr_Int32.py -v %s -i %s -o %s -a %s",
+               scriptdir,
+               segment_file,
+               mosaic_file,
+               segment_tif,
+               "DN"
+))
+
+# Calculate processing time
+timeDiff <- Sys.time() - startTime; cat("The Processing time is", format(timeDiff), "\n")
